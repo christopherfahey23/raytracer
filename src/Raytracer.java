edf9ef34eda65fd0d6023068422451226874dfab
christopherfahey23 <christopherfahey23@gmail.com>
2022-02-20 12:40:18 -0500

    Initial commit. Test writing to .ppm file.

---
+++ src/Raytracer.java
@@ -0,0 +1,27 @@
+import java.io.FileWriter;
+
+public class Raytracer {
+    public static final int WIDTH = 256;
+    public static final int HEIGHT = 256;
+
+
+    public static void main(String[] args) throws Exception {
+        try {
+            FileWriter f = new FileWriter("output.ppm");
+
+            f.write("P3\n");
+            f.write(WIDTH + " " + HEIGHT + "\n");
+            f.write(255 + "\n");
+
+            for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH; j++) {
+                    f.write("255 0 0\n");
+                }
+            }
+
+            f.close();
+        } catch (Exception e) {
+            System.out.println("Error writing file");
+        }
+    }
+}
